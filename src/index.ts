472c992660ef1445a73a5d05357a120550cf2cc7
klemela <klemela>
2018-08-21 09:51:23 +0300

    Did it work even before the exports?

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Logger } from "./logger";
-import { Config } from "./config";
-import { RestClient } from "./rest-client";
-
-exports.Logger = Logger;
-exports.Config = Config;
-exports.RestClient = RestClient;
