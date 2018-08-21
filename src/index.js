7e9bd742ed6897e5ca70989e19edbd7d89aeed2f
klemela <klemela>
2018-08-21 09:48:05 +0300

    Export classes

---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var logger_1 = require("./logger");
+var config_1 = require("./config");
+var rest_client_1 = require("./rest-client");
+exports.Logger = logger_1.Logger;
+exports.Config = config_1.Config;
+exports.RestClient = rest_client_1.RestClient;
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { Logger } from "./logger";
+import { Config } from "./config";
+import { RestClient } from "./rest-client";
+
+exports.Logger = Logger;
+exports.Config = Config;
+exports.RestClient = RestClient;
