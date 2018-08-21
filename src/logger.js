35bd9c391d61346ecd1b09835d76cfd238d4de11
klemela <klemela>
2018-08-21 09:40:34 +0300

    Change the module name and commit js code

---
+++ src/logger.js
@@ -0,0 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var path = require('path');
+var winston = require('winston');
+var Logger = /** @class */ (function () {
+    function Logger() {
+    }
+    Logger.getLogger = function (filepath) {
+        var filename = path.basename(filepath);
+        return new (winston.Logger)({
+            transports: [
+                new (winston.transports.Console)({
+                    timestamp: function () {
+                        return new Date();
+                    },
+                    formatter: function (options) {
+                        var d = options.timestamp();
+                        var datestring = (d.getFullYear()
+                            + "-" + ("0" + (d.getMonth() + 1)).slice(-2))
+                            + "-" + ("0" + d.getDate()).slice(-2)
+                            + " " + ("0" + d.getHours()).slice(-2)
+                            + ":" + ("0" + d.getMinutes()).slice(-2)
+                            + ":" + ("0" + d.getSeconds()).slice(-2)
+                            + "," + ("0" + d.getMilliseconds()).slice(-3);
+                        return '[' + datestring + '] '
+                            + options.level.toUpperCase() + ': '
+                            + (options.message ? options.message : '')
+                            + (options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '')
+                            + ' (in ' + filename + ')';
+                    }
+                })
+            ]
+        });
+    };
+    return Logger;
+}());
+exports.Logger = Logger;
