c8159939642862df8cf551af33dbcb98c822e72b
Taavi Hupponen <taavi.hupponen@csc.fi>
2018-10-08 16:34:59 +0300

    Add latestSession field to user

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,14 +1,16 @@
 export default class User {
-    public auth: string;
-    public username: string;
-    public mail: string;
-    public organization: string;
-    public name: string;
-    public termsAccepted: string;
-    public termsVersion: number;
+  public auth: string;
+  public username: string;
+  public mail: string;
+  public organization: string;
+  public name: string;
+  public termsAccepted: string;
+  public termsVersion: number;
 
-    public version: number;
-    public created: string;
-    public modified: string;
-    public accessed: string;
+  public version: number;
+  public created: string;
+  public modified: string;
+  public accessed: string;
+
+  public latestSession: string;
 }
