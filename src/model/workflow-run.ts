5094d8afc6096200706587d9842e4c2aba0dfe1d
Petri Klemela <petri.klemela@csc.fi>
2019-11-25 10:37:06 +0200

    Allow client to set shorter timeouts

--- src/model/workflow-run.ts
+++ src/model/workflow-run.ts
@@ -1,14 +1,34 @@
-import { JobState } from "./job";
 import WorkflowJob from "./workflow-job";
 
-export default class Job {
+export enum WorkflowState {
+  New = "NEW",
+  Running = "RUNNING",
+  Completed = "COMPLETED",
+  Failed = "FAILED",
+  Error = "ERROR",
+  Cancelled = "CANCELLED",
+  Cancelling = "CANCELLING",
+  Draining = "DRAINING",
+}
+
+export enum DrainMode {
+  Ignore = "IGNORE",
+  BailOut = "BAIL_OUT",
+  Drain = "Drain",
+}
+
+export default class WorkflowRun {
   created: string;
   endTime: string;
   workflowRunId: string;
   sessionId: string;
-  state: JobState;
+  state: WorkflowState;
   stateDetail: string;
   createdBy: string;
   name: string;
   workflowJobs: WorkflowJob[];
+  runningTimeout: number;
+  cancellingTimeout: number;
+  drainingTimeout: number;
+  onError: DrainMode;
 }
