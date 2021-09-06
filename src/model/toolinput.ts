063a7cbef108423b87b696b3128a99b30f37181f
Taavi Hupponen <taavi.hupponen@csc.fi>
2021-09-06 12:14:59 +0300

    Update set ToolInput type as ToolInputType

--- src/model/toolinput.ts
+++ src/model/toolinput.ts
@@ -1,8 +1,9 @@
 import Name from "./name";
+import ToolInputType from "./toolinputtype";
 export default class ToolInput {
-    description: string;
-    meta: boolean;
-    name: Name;
-    optional: boolean;
-    type: any;
+  description: string;
+  meta: boolean;
+  name: Name;
+  optional: boolean;
+  type: ToolInputType;
 }
